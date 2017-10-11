cd765e68c83738f685aa711c3d85bf6f99beabe9
Антон Меренков <a.merenkov89@mail.ru>
2017-10-11 11:13:13 +0300

    Ошибка формирования ссылок в облаке тэгов

--- widgets/views/popular-tags.php
+++ widgets/views/popular-tags.php
@@ -12,29 +12,15 @@ use yii\helpers\Url;
     <div class="widget widget-tags">
         <div id="tags"></div>
         <script>
-            $('#tags').jQCloud(<?=Json::encode($tags)?>, {
+            $('#tags').jQCloud(<?=json_encode($tags)?>, {
                 colors: ["#67c1f5", "#5fb1e0", "#57a0cb", "#4f8fb6", "#477ea0", "#3f6e8b", "#375d76", "#2f4c61"],
                 fontSize: {
                     from: 0.18,
                     to: 0.05
                 },
-                delay: 50
+                delay: 50,
+                encodeURI: false
             });
         </script>
     </div>
-
-    <!--<nav>
-        <ul>
-            <?/* foreach ($tags as $tag) : */?>
-                <li<?/* if ($tag[ 'active' ]) : */?> class="active"<?/* endif; */?>>
-                    <a href="<?/*=Url::to([
-                        'site/index',
-                        "category_id" => Yii::$app->request->get('category_id', null),
-                        "channel_id" => Yii::$app->request->get('channel_id', null),
-                        "query" => $tag[ 'text' ]
-                    ])*/?>"><?/*=$tag[ 'text' ] */?></a>
-                </li>
-            <?/* endforeach; */?>
-        </ul>
-    </nav>-->
 <? endif; ?>
