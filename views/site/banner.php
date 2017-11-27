b24223bbf13784e5a2cde79b15fb264b509a4607
Антон Меренков <a.merenkov89@mail.ru>
2017-11-27 13:20:42 +0300

    Отдельный файл для баннера

---
+++ views/site/banner.php
@@ -0,0 +1,28 @@
+<h4>Добро пожаловать на сайт самого хайпового видео с YouTUBE! У нас есть:</h4>
+
+<div class="row">
+    <div class="col-md-4 col-xs-6">
+        <i class="glyphicon glyphicon-refresh" style="color: #ff4539"></i>
+        <p>Живой ТОП видео Ютуба</p>
+    </div>
+    <div class="col-md-4 col-xs-6">
+        <i class="glyphicon glyphicon-user" style="color: #ee7624"></i>
+        <p>ТОП блоггеров Ютуба</p>
+    </div>
+    <div class="col-md-4 col-xs-6">
+        <i class="glyphicon glyphicon-eye-open" style="color: #f4c400"></i>
+        <p>Быстрый и удобный просмотр</p>
+    </div>
+    <div class="col-md-4 col-xs-6">
+        <i class="glyphicon glyphicon-sort-by-attributes" style="color: #00833f"></i>
+        <p>Возможность сортировки каналов и поиска</p>
+    </div>
+    <div class="col-md-4 col-xs-6">
+        <i class="glyphicon glyphicon-exclamation-sign" style="color: #0099a9"></i>
+        <p>Инфа о блоггерах, о которых даже не подозревал</p>
+    </div>
+    <div class="col-md-4 col-xs-6">
+        <i class="glyphicon glyphicon-search" style="color: #86339a"></i>
+        <p>Выбор категории и каналов по интересам</p>
+    </div>
+</div>
